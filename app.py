702c4d0ff9fd1fa778e275764e29fffb5ba7164d
Daisy-McG <mcgirr.daisy@gmail.com>
2020-12-14 21:19:46 +0000

    Set up user registration page template, routing and navigation

--- app.py
+++ app.py
@@ -2,17 +2,65 @@ import os
 from flask import (
     Flask, flash, render_template, redirect,
     request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
+
+
+"""
+Renders home page template when going to the main website link
+"""
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("index.html")
+
+
+"""
+Sign up - This function allows the user to register on the Sign Up page.
+If the username already exists in the DB a flash message will
+    display to alert the user.
+If the username does not exist and registration was successful,
+    the user will be logged in and a flash message displayed to verify it
+    was successful.
+"""
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("This user name already exists.")
+            return redirect(url_for("signup"))
+
+        newUser = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password")),
+            "name": request.form.get("name").lower()
+        }
+        mongo.db.users.insert_one(newUser)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        render_template("profile.html")
+
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
